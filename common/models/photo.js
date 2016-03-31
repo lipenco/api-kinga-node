1bd0df5ff552ce24ccc563c2c92e8cd3544dffc1
Magdalena Lipińska <mag.lipinska@gmail.com>
2016-03-31 23:10:19 +0100

    Add img uploader

--- common/models/photo.js
+++ common/models/photo.js
@@ -1,5 +1,19 @@
+var cloudinary = require('cloudinary');
+
+cloudinary.config({
+  cloud_name: 'hpymbyjxq',
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET
+});
 
 module.exports = function(Photo) {
 
+  Photo.observe("before save", (ctx, next) => {
+    let data = ctx.instance || ctx.data;
+    cloudinary.uploader.upload(ctx.instance.data, function(result) {
+      data.url = result.url;
+      next();
+    });
+  });
 
 };
