151c0464b4c300485bd0a50fbbe6d398f8a7e996
Magdalena Lipińska <mag.lipinska@gmail.com>
2016-04-03 21:22:50 +0100

    add datasource

--- server/datasources.production.js
+++ server/datasources.production.js
@@ -1,5 +1,11 @@
 module.exports = {
   db: {
     url: process.env.MONGOLAB_URI
+  },
+  storage: {
+    name: "storage",
+    connector: "loopback-component-storage",
+    provider: "filesystem",
+    root: "storage"
   }
 };
