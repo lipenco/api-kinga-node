439dc81524edabff4ed533a78f15a862c70a608d
Magdalena Lipińska <mag.lipinska@gmail.com>
2016-04-17 21:56:06 +0100

    AChange cretelogic

--- common/models/photo.js
+++ common/models/photo.js
@@ -6,23 +6,39 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_SECRET
 });
 
+Photo.beforeCreate = function(next, modelInstance) {
+  let data = modelInstance;
+  console.log("here", modelInstance)
+  return cloudinary.uploader.upload(
+    "https://kinga-api.herokuapp.com/" + data.tempurl, function(result) {
+      if (result) {
+        data.url = result.url;
+        data.public_id = result.public_id;
+        next();
+      } else {
+        next();
+      }
+  });
+  next();
+};
+
 module.exports = function(Photo) {
 
-  Photo.observe("before create", (ctx, next) => {
-    console.log(ctx.instance)
-    let data = ctx.instance || ctx.data;
-
-    return cloudinary.uploader.upload(
-      "https://kinga-api.herokuapp.com/" + data.tempurl, function(result) {
-        if (result) {
-          data.url = result.url;
-          data.public_id = result.public_id;
-          next();
-        } else {
-          next();
-        }
-    });
-
-  });
+  // Photo.observe("before create", (ctx, next) => {
+  //   console.log(ctx.instance)
+  //   let data = ctx.instance || ctx.data;
+  //
+  //   return cloudinary.uploader.upload(
+  //     "https://kinga-api.herokuapp.com/" + data.tempurl, function(result) {
+  //       if (result) {
+  //         data.url = result.url;
+  //         data.public_id = result.public_id;
+  //         next();
+  //       } else {
+  //         next();
+  //       }
+  //   });
+  //
+  // });
 
 };
