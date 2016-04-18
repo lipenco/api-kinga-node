ac5fa0b17fdfcf50038a42e55ea538b3771e5017
Magdalena Lipińska <mag.lipinska@gmail.com>
2016-04-18 22:34:23 +0100

    try

--- common/models/project.js
+++ common/models/project.js
@@ -1,7 +1,7 @@
-var Photo = require('./photo.js');
+var Photo = req.app.models.Photo;
 module.exports = function(Project) {
   Project.observe('before delete', function (ctx, next) {
-    console.log("before destory triggeres", ctx.where.id, ctx.where, next)
+    console.log("before destory triggeres", ctx.where.id)
 
     Photo.destroyAll({projectId : ctx.where.id})
     next();
