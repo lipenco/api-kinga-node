af2c787f03e575e880fdce37660fa9a888149c0a
Magdalena Lipińska <mag.lipinska@gmail.com>
2016-04-18 22:26:48 +0100

    refactor

--- common/models/project.js
+++ common/models/project.js
@@ -1,9 +1,9 @@
 var Photo = require('./photo.js');
 module.exports = function(Project) {
   Project.observe('before delete', function (ctx, next) {
-    console.log("before destory triggeres", ctx)
+    console.log("before destory triggeres", ctx.where.id, ctx.where, next)
 
-    Photo.destroyAll({projectId : ctx})
+    Photo.destroyAll({projectId : ctx.where.id})
     next();
   });
 
