47018adab5355200aba27b56a0fc9b68204354f0
Magdalena Lipińska <mag.lipinska@gmail.com>
2016-04-18 22:40:41 +0100

    ds

--- common/models/project.js
+++ common/models/project.js
@@ -1,6 +1,9 @@
-var Photo = req.app.models.Photo;
+var loopback = require('loopback');
+var Photo = loopback.getModel("Photo")
+
 module.exports = function(Project) {
   Project.observe('before delete', function (ctx, next) {
+    // ctx.photos.destroyAll()
     console.log("before destory triggeres", ctx.where.id)
 
     Photo.destroyAll({projectId : ctx.where.id})
